71b2f10e1e49e1562f166830a532cf6b9556cdab
Aaron Romel <lynques.io@gmail.com>
2018-02-11 18:19:30 -0500

    Add jasmine/karma testing

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
+declare var __karma__: any;
+declare var require: any;
+
+// Prevent Karma from running prematurely.
+__karma__.loaded = () => { };
+
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts/);
+// And load the modules.
+context.keys().map(context);
+// Finally, start Karma to run the tests.
+__karma__.start();
