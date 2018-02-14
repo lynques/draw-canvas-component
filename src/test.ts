0ee9ae968305d911310776361d274cb1ac0d54c3
Aaron Romel <aromel@forbes.com>
2018-02-14 15:36:55 -0500

    ignore empty block; test.ts

--- src/test.ts
+++ src/test.ts
@@ -3,6 +3,7 @@ declare var __karma__: any;
 declare var require: any;
 
 // Prevent Karma from running prematurely.
+// tslint-disable-next-line:no-empty
 __karma__.loaded = () => { };
 
 // Then we find all the tests.
